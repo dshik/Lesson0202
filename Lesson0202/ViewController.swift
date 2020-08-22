c6b8cffe9a580dfc17e966f7102971f5e57bd5c8
dshik <dshik@mail.ru>
2020-08-22 15:32:14 +0300

    Начинаем задание урока 2 курса 2

---
+++ Lesson0202/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson0202
+//
+//  Created by Дмитрий Шикунов on 22.08.2020.
+//  Copyright © 2020 Дмитрий Шикунов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
